71c0e666b2bec6e88394fd4e00bcefd66391fd66
Jon Fus <jonfus123@hotmail.com>
2020-08-18 23:23:09 -0400

    Added Tile class and TileMap ScriptableObject
    - This will allow ineditor setting of hex sprite tile names

---
+++ Assets/Tile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Conquest
+{
+    /// <summary>
+    /// Container for a 2D Tile (Hex) sprite and name
+    /// </summary>
+    [Serializable]
+    public class Tile
+    {
+        public string name;
+        public Sprite sprite;
+    }
+}
+
---
+++ Assets/TileMap.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Conquest
+{
+    /// <summary>
+    /// ScriptableObject. An array of Tile objects. Use to get Tiles by index or name.
+    /// TileMap contains an internal Dictionary pairing Tile names to index.
+    /// </summary>
+    [CreateAssetMenu(fileName = "TileMap", menuName = "Conquest/ScriptableObjects/TileMap", order = 0)]
+    public class TileMap : ScriptableObject, ISerializationCallbackReceiver
+    {
+        [NonSerialized]
+        private Dictionary<string, int> m_nameToID;
+
+        public Tile[] tiles;
+
+        public Tile GetTile(int index)
+        {
+            return tiles[index];
+        }
+
+        public Tile GetTileByName(string name)
+        {
+            if (m_nameToID.TryGetValue(name, out int value))
+            {
+                return tiles[value];
+            }
+            return null;
+        }
+
+        public void OnBeforeSerialize()
+        {
+
+        }
+
+        public void OnAfterDeserialize()
+        {
+            m_nameToID = new Dictionary<string, int>();
+            for (int i = 0; i < tiles.Length; i++)
+            {
+                m_nameToID.Add(tiles[i].name, i);
+            }
+        }
+    }
+}
+
