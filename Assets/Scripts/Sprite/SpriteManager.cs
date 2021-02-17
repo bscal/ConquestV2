4c66a00f72b3e87950907bec511ad78f43a963a2
Jon Fus <jonfus123@hotmail.com>
2021-02-17 17:58:02 -0500

    Changed SpriteManager to be a ScriptableObject accessed through gamemanager

--- Assets/Scripts/Sprite/SpriteManager.cs
+++ Assets/Scripts/Sprite/SpriteManager.cs
@@ -2,19 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpriteManager : MonoBehaviour
+[CreateAssetMenu(fileName = "Sprites", menuName = "Conquest/Sprites")]
+public class SpriteManager : ScriptableObject
 {
-
-    public static SpriteManager Singleton { get; private set; }
 
     public Sprite arrow;
     public Sprite neutral;
     public Sprite cross;
-
-    private void Awake()
-    {
-        Singleton = this;
-    }
 
 
 }
